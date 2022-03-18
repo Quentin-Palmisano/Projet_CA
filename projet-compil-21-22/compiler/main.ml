9ef7d3b8840fc03dd8463ce1564e70fbe13dee57
Robin <louis.robin94@gmail.com>
2022-03-18 13:07:33 +0100

    nettoyeur

--- projet-compil-21-22/compiler/main.ml
+++ projet-compil-21-22/compiler/main.ml
@@ -1,9 +1,23 @@
+open String
+
+let nettoyeur s = 
+  let l = split_on_char '\n' s in
+  let temp = List.fold_left (fun ret e ->
+    if (contains e ':')
+    then
+      match ret with
+        | [] -> e::ret
+        | hd::tl -> if contains hd ':' then e::tl else e::ret
+    else e::ret 
+  ) [] l in
+  concat "\n" (List.rev temp)
+
 let process file =
   let ic = open_in file in 
   let lexbuf = Lexing.from_channel ic in
   let ast = Parser.prog Lexer.token lexbuf in
   let oc = open_out "../bytecode/test1.txt" in 
-  let s = (Eval.evalInst ast 1) in
+  let s = nettoyeur(Eval.evalInst ast 1) in
   begin
   Printf.fprintf oc "%s\tSTOP" s;
   close_out oc;
